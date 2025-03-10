db56ae7c4a8e4332a52cd73d9c559856c2ceb9de
mwirigikelvinkubai <mwirigiomukombiti@gmail.com>
2025-03-10 21:03:17 +0300

    superbowl

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(records) {
+    // im trying to locate the first record where result is "W"
+    const winningRecord = records.find(record => record.result === "W");
+  
+    // If a winning record is found, return the year; otherwise, return undefined
+    return winningRecord ? winningRecord.year : undefined;
+  }
